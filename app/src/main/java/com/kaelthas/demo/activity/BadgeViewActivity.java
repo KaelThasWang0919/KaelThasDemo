7e5f06f70cf8d40b8ca1d770b4d858f787b08c6d
KaelThas.Wang <kaelthas@KaelThasWang.local>
2017-03-20 10:49:26 +0800

    FIX  文件名称

--- app/src/main/java/com/kaelthas/demo/activity/BadgeViewActivity.java
+++ app/src/main/java/com/kaelthas/demo/activity/BadgeViewActivity.java
@@ -11,6 +11,7 @@ import com.kaelthas.demo.base.BaseActivity;
 
 public class BadgeViewActivity extends BaseActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
